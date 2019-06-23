7d902852c640700837b010348c873dd332b3a459
oussema95 <37015115+oussema95@users.noreply.github.com>
2019-06-24 00:45:11 +0200

    Update construction-formation-modele.py

--- construction-formation-modele.py
+++ construction-formation-modele.py
@@ -18,8 +18,8 @@ def load_data(args):
     data_df = pd.read_csv(os.path.join(os.getcwd(), args.data_dir, 'driving_log.csv'), names=['center', 'left', 'right', 'steering', 'throttle', 'reverse', 'speed'])
     X = data_df[['center', 'left', 'right']].values
     y = data_df['steering'].values
-    X_train, X_valid, y_train, y_valid = train_test_split(X, y, test_size=args.test_size, random_state=0)
-    return X_train, X_valid, y_train, y_valid
+    X_t, X_v, y_t, y_v = train_test_split(X, y, test_size=args.test_size, random_state=0)
+    return X_train, X_v, y_t, y_v
 
 
 def build_model(args):
@@ -30,10 +30,10 @@ def build_model(args):
     model = Sequential()
     model.add(Lambda(lambda x: x/127.5-1.0, input_shape=INPUT_SHAPE))
     model.add(Conv2D(24, 5, 5, activation='elu', subsample=(2, 2)))
-    model.add(Conv2D(36, 5, 5, activation='elu', subsample=(2, 2)))
-    model.add(Conv2D(48, 5, 5, activation='elu', subsample=(2, 2)))
+    model.add(Conv2D(36, 4, 4, activation='elu', subsample=(2, 2)))
+    model.add(Conv2D(48, 3, 3, activation='elu', subsample=(2, 2)))
     model.add(Conv2D(64, 3, 3, activation='elu'))
-    model.add(Conv2D(64, 3, 3, activation='elu'))
+    model.add(Conv2D(72, 3, 3, activation='elu'))
     model.add(Dropout(args.keep_prob))
     model.add(Flatten())
     model.add(Dense(100, activation='elu'))
@@ -45,7 +45,7 @@ def build_model(args):
     return model
 
 
-def train_model(model, args, X_train, X_valid, y_train, y_valid):
+def train_model(model, args, X_t, X_v, y_t, y_v):
     """
     entrainer le model
     """
@@ -55,7 +55,7 @@ def train_model(model, args, X_train, X_valid, y_train, y_valid):
     model.compile(loss='mean_squared_error', optimizer=Adam(lr=args.learning_rate))
 
 
-    model.fit_generator(batch_generator(args.data_dir, X_train, y_train, args.batch_size, True),args.samples_per_epoch,args.nb_epoch,max_q_size=1,validation_data=batch_generator(args.data_dir, X_valid, y_valid, args.batch_size, False),nb_val_samples=len(X_valid),callbacks=[checkpoint],verbose=1)
+    model.fit_generator(batch_generator(args.data_dir, X_t, y_t, args.batch_size, True),args.samples_per_epoch,args.nb_epoch,max_q_size=1,validation_data=batch_generator(args.data_dir, X_v, y_v, args.batch_size, False),nb_val_samples=len(X_v),callbacks=[checkpoint],verbose=1)
 
 def s2b(s):
     """
@@ -70,14 +70,14 @@ def main():
     validation et entrainement du modele
     """
     parser = argparse.ArgumentParser(description='le programme du clonage du comportement ')
-    parser.add_argument('-d', help='data directory',        dest='data_dir',          type=str,   default='data')
-    parser.add_argument('-t', help='test size fraction',    dest='test_size',         type=float, default=0.2)
-    parser.add_argument('-k', help='drop out probability',  dest='keep_prob',         type=float, default=0.5)
-    parser.add_argument('-n', help='number of epochs',      dest='nb_epoch',          type=int,   default=10)
-    parser.add_argument('-s', help='samples per epoch',     dest='samples_per_epoch', type=int,   default=20000)
-    parser.add_argument('-b', help='batch size',            dest='batch_size',        type=int,   default=40)
-    parser.add_argument('-o', help='save best models only', dest='save_best_only',    type=s2b,   default='true')
-    parser.add_argument('-l', help='learning rate',         dest='learning_rate',     type=float, default=1.0e-4)
+    parser.add_argument('-d', help='donnees controle ',   type=str,   default='data')
+    parser.add_argument('-t', help='test size fraction',       type=float, default=0.2)
+    parser.add_argument('-k', help='drop out probability',        type=float, default=0.5)
+    parser.add_argument('-n', help='nombre des  epochs',            type=int,   default=10)
+    parser.add_argument('-s', help='echantiollinage ',   type=int,   default=20000)
+    parser.add_argument('-b', help='taille',       type=int,   default=40)
+    parser.add_argument('-o', help='enregistrer le modele ',type=s2b,   default='true')
+    parser.add_argument('-l', help='apprendre ',   type=float, default=1.0e-4)
     args = parser.parse_args()
     print('-' * 30)
     print('Parameters')

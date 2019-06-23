663c1dcb9253c692970d37f1d68056b6c9e6ae2f
oussema95 <37015115+oussema95@users.noreply.github.com>
2019-06-24 01:23:59 +0200

    Update test du modele.py

--- test du modele.py
+++ test du modele.py
@@ -1,12 +1,13 @@
+import socketio
+import eventlet
+import eventlet.wsgi
 import argparse
 import base64
 from datetime import datetime
 import os
 import shutil
 import numpy as np
-import socketio
-import eventlet
-import eventlet.wsgi
+
 from PIL import Image
 from flask import Flask
 from io import BytesIO
